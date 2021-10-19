3fb123a5ba96b1d0ca407870b478746634d6946b
chiragrajoria <chiragrajoria05@gmail.com>
2021-10-19 18:08:29 +0530

    committed

---
+++ client/src/Layout/Home.layout.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//components
+import Navbar from "../Components/Navbar";
+import FoodTab from "../Components/FoodTab";
+
+const HomeLayout = (props) => {
+    return <>
+        <div className="container mx-auto  lg:px-20">
+            <Navbar />
+          
+            {props.children}
+            
+        </div>
+        
+        <FoodTab/>
+    </>
+
+};
+
+export default HomeLayout;
