37051acf7ad1ff6d7d289ec6542602ad394fe6d6
chiragrajoria <chiragrajoria05@gmail.com>
2021-10-19 20:52:57 +0530

    redux

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React ,{useEffect,useState} from "react";
+
+import { useSelector } from "react-redux";
 
 //components
 import DeliveryCarousal from "./DeliveryCarousal";
 
+
 const Delivery=()=>{
+    const [restaurantList,setRestaurantList]=useState([]);
+
+    const reduxState=useSelector(
+        (globalStore)=>globalStore.restaurant.restaurants
+    );
+    useEffect(()=>{
+        setRestaurantList(reduxState.restaurant);
+    },[reduxState.restaurants]);
     return (
         <>
         <DeliveryCarousal/>
